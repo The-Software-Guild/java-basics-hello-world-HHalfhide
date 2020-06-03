cdfa30890842dfe81b5e8fed8e8beb723591f301
HHalfhide <harryhalfhide@gmail.com>
2020-06-03 15:05:12 +0100

    Hello World in NetBeans

---
+++ .github/helloWorld/src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author harry
+ */
+package mthree.helloworld;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
